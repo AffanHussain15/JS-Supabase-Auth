4dfd449df038cae2e7fcbf4cb9b4b2fd90b9721b
AffanHussain15 <affanhussain015@gmail.com>
2025-01-24 23:21:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const password = document.getElementById("password");
+const email = document.getElementById("email");
+
+const supabaseUrl = "https://dpsnymvjqugamqrfrrys.supabase.co";
+const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRwc255bXZqcXVnYW1xcmZycnlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcyODA5MzQsImV4cCI6MjA1Mjg1NjkzNH0.7g1hU78xsVqiVf7c-i2QBteTloi_N-vRvomr6Tgxjtg`;
+const supaBase = supabase.createClient(supabaseUrl, supabaseKey);
+
+async function signup(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.email.value;
+  const password = form.password.value;
+
+  console.log("user email", email);
+  console.log("user password", password);
+  const { data, error } = await supaBase.auth.signUp({
+    email: email,
+    password: password,
+  });
+  if (error) {
+    console.error("Signup Error:", error.message);
+    alert(`Error: error`);
+  } else {
+    console.log("Signup Successful:", data);
+    alert("Signup successful! Please verify your email.");
+  }
+  console.log("data", data);
+  console.log("error", error);
+};
